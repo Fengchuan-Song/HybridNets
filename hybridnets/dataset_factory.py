ac2457db902cbb4238af704662f618e6b6c9ffcb
Fengchuan-Song <1319825810@qq.com>
2026-05-19 20:31:22 +0800

    update

--- hybridnets/dataset_factory.py
+++ hybridnets/dataset_factory.py
@@ -1,12 +1,10 @@
-from hybridnets.custom_dataset import CustomDataset
-from hybridnets.dataset import BddDataset
-from hybridnets.waterscenes_dataset import WaterScenesDataset
-
-
 def get_dataset_class(params):
     dataset_type = (params.dataset.get('type') or 'bdd100k').lower()
     if dataset_type in {'waterscenes', 'water_scenes'}:
+        from hybridnets.waterscenes_dataset import WaterScenesDataset
         return WaterScenesDataset
     if dataset_type == 'custom':
+        from hybridnets.custom_dataset import CustomDataset
         return CustomDataset
+    from hybridnets.dataset import BddDataset
     return BddDataset
